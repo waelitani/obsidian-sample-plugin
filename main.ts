e66e0ed3b4304c19798055a36fd65cff1935ecca
waelitani <wak.itani.96@gmail.com>
2024-06-01 10:12:18 -0400

    Minimum Viable Plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,153 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { DataSet, Network, Edge } from 'vis-network/standalone';
 
-// Remember to rename these classes and interfaces!
+interface Task {
+  id: string;
+  name: string;
+  dependencies: string[];
+}
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: 'default'
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.addRibbonIcon('dice', 'Task Dependencies', () => {
+      this.showTaskDependencyGraph();
+    });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.addSettingTab(new MyPluginSettingTab(this.app, this));
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async showTaskDependencyGraph() {
+    const tasks: Task[] = await this.getTasksFromNotes();
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    const container = document.createElement('div');
+    container.classList.add('task-dependency-graph');
+    container.style.width = '100%';
+    container.style.height = '400px';
+    container.style.border = '1px solid black';
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    const modal = new Modal(this.app);
+    modal.titleEl.setText('Task Dependency Graph');
+    modal.contentEl.appendChild(container);
 
-	onunload() {
+    this.renderGraph(container, tasks);
 
-	}
+    modal.open();
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async getTasksFromNotes(): Promise<Task[]> {
+    const tasks: Task[] = [];
+    const taskIds: Set<string> = new Set();
+    let uniqueId = 1;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    const files = this.app.vault.getMarkdownFiles();
+    for (const file of files) {
+      const content = await this.app.vault.cachedRead(file);
+      const lines = content.split('\n');
+      lines.forEach((line: string) => {
+        const taskMatch = line.match(/- \[[ x]\] (.+)/);
+        if (taskMatch) {
+          const taskName = taskMatch[1].trim();
+
+          const idMatch = taskName.match(/\[id:(\w+)\]/);
+          const taskId = idMatch ? idMatch[1] : `task${uniqueId++}`;
+
+          const dependsOnMatch = taskName.match(/\[dependsOn:(\w+)\]/g);
+          const dependsOn = dependsOnMatch ? dependsOnMatch.map(match => match.slice(11, -1)) : [];
+
+          if (!taskIds.has(taskId)) {
+            taskIds.add(taskId);
+            tasks.push({
+              id: taskId,
+              name: taskName.replace(/\[[^\]]+\]/g, '').trim(),
+              dependencies: dependsOn,
+            });
+          }
+        }
+      });
+    }
+
+    console.log('Tasks:', tasks);
+    return tasks;
+  }
+
+  renderGraph(container: HTMLElement, tasks: Task[]) {
+    const nodes = new DataSet(
+      tasks.map((task) => ({ id: task.id, label: task.name }))
+    );
+    console.log('Nodes:', nodes);
+
+    const edges = new DataSet<Edge>(
+      tasks.flatMap((task) =>
+        task.dependencies.map((dep) => ({ from: dep, to: task.id }))
+      )
+    );
+    console.log('Edges:', edges);
+
+    const data = {
+      nodes: nodes,
+      edges: edges,
+    };
+
+    const options = {
+      layout: {
+        hierarchical: {
+          direction: 'LR',
+          sortMethod: 'directed',
+        },
+      },
+      edges: {
+        arrows: 'to',
+      },
+    };
+
+    new Network(container, data, options);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class MyPluginSettingTab extends PluginSettingTab {
+  plugin: MyPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+    const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    containerEl.createEl('h2', { text: 'Settings for my plugin.' });
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue(this.plugin.settings.mySetting)
+        .onChange(async (value) => {
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
